8d1065cd77c4b631afe32a63ece076a45ef75a91
VloneMe <francisntima@yahoo.com>
2023-05-24 09:07:18 -0700

    main shell update

---
+++ main_shell.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * main - main shell function
+ *
+ * Return: 0 on (success)
+ */
+int main(void)
+{
+	ssize_t len = 0;
+	char *buff = NULL, *value, *pathname, **arv;
+	size_t size = 0;
+	list_path *head = '\0';
+	void (*f)(char **);
+
+	signal(SIGINT, handle_signal);
+	while (len != EOF)
+	{
+		_prompt();
+		len = getline(&buff, &size, stdin);
+		_handleEOF(len, buff);
+		arv = splitstring(buff, " \n");
+		if (!arv || !arv[0])
+			execute(arv);
+		else
+		{
+			value = _getenv("PATH");
+			head = linkpath(value);
+			pathname = _which(arv[0], head);
+			f = handle_build(arv);
+			if (f)
+			{
+				free(buff);
+				f(arv);
+			}
+			else if (!pathname)
+				execute(arv);
+			else if (pathname)
+			{
+				free(arv[0]);
+				arv[0] = pathname;
+				execute(arv);
+			}
+		}
+	}
+	free_list(head);
+	freearv(arv);
+	free(buff);
+	return (0);
+}
